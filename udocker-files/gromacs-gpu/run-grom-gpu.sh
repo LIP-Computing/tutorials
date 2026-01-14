f17190a18fa16d2e4d2067431fe30280f1c90e02
mariojmdavid <mariojmdavid@gmail.com>
2026-01-14 17:37:25 +0000

    fix path

--- udocker-files/gromacs-gpu/run-grom-gpu.sh
+++ udocker-files/gromacs-gpu/run-grom-gpu.sh
@@ -2,5 +2,5 @@
 
 . /usr/local/gromacs/bin/GMXRC.bash
 
-gmx mdrun -s /home/user/input/md.tpr -e $EDR -x $XTC -o $TRR -g $LOG \
+gmx mdrun -s /home/user/md.tpr -e $EDR -x $XTC -o $TRR -g $LOG \
     -maxh 0.50 -resethway -noconfout -nsteps 10000 -nt 8 -pin on
